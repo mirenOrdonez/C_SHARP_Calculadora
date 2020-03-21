8a71b71248e56f06c403ebb4e113c33d9b6080c4
Miren Ordóñez <miren.ordonez@gmail.com>
2020-03-21 11:53:01 +0100

    Con la raíz cuadrada, el exponente y la reset. Interfaz bien.

---
+++ Calculadora_C/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_C
+{
+    public partial class Form1 : Form
+    {
+        //Declaro las variables de instancia
+        //operando 1
+        double operando1 = 0;
+
+        //En el string operacion guardo la operación que ha sido pulsada
+        String operacion = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            if (label1.Text == "0")
+            {
+                label1.Text = boton.Text;
+            }
+            else { 
+                label1.Text = label1.Text + boton.Text;
+            }
+            
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            operacion = boton.Text;
+            operando1 = Convert.ToDouble(label1.Text);
+            label1.Text = "0";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            double operando2 = Convert.ToDouble(label1.Text);
+            double resultado = 0;
+            if (operacion == "+")
+            {
+                resultado = operando1 + operando2;
+            }
+            else if (operacion == "-") {
+                resultado = operando1 - operando2;
+            }
+            label1.Text = Convert.ToString(resultado);
+        }
+    }
+}
